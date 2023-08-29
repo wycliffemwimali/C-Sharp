9a31a9f99d9f8513bc620ebe062bdeec04214559
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-08-29 19:14:21 +0300

    c#

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello Mwimali");
+Console.WriteLine("congratulations");
+Console.WriteLine("");
+Console.WriteLine("you wrote your first code");
