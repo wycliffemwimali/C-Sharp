a3ef9be1b621ec1c81f1d7f240bddb03091e0d1e
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-08-29 22:10:08 +0300

    interpolation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,3 +9,8 @@ Console.Write(@"c:\invoices");
 Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
 // User command to run an application
 Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//interpolation
+string message = "update to windows";
+int version = 11;
+Console.WriteLine($"{message} {version}");
